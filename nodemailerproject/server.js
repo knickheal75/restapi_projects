108a1f0eb48e46ed39a9fadd6dc4d78d3b8d782f
knickheal75 <blitzkrieg0705@gmail.com>
2019-03-26 08:54:45 +0530

    nodemailer implementation gmail

---
+++ nodemailerproject/server.js
@@ -0,0 +1,82 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser");
+require("./dbConnect");
+const User=require("./models/user");
+require("path");
+const port=process.env.PORT||3000;
+const nodemailer=require("nodemailer");
+const Email="nikhielmiend@gmail.com"
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static(__dirname+"/views"));
+
+app.post("/",function(req,res){
+    var user=new User();
+
+    user.name=req.body.name;
+    user.email=req.body.email;
+    user.phone=req.body.phone;
+    user.message=req.body.message;
+    
+    //NODEMAILER DOCUMENTTION START
+    
+let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: Email, // check top
+          pass: "" //ENTER THE PASSWORD HERE
+        }
+      });
+
+let mailOptions = {
+        from: `"Nikhil Mind" <${Email}>`, // sender address
+        to: "nikhielmiend@gmail.com, `${req.body.email}`", // list of receivers
+        subject: "contact info", // Subject line
+        text: "How are you", // plain text body
+        html: `<b>Contact Information </b> <hr> <h2>name:` + req.body.name+`<hr>phone:`+req.body.phone+`<hr>email:`+req.body.email+
+        `<hr>message:`+req.body.message+`</h2>`
+      };
+
+      transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        console.log('message sent: %s', info.messageId);
+    })
+    
+    //NODEMAILER DOCUMENTTION END
+    
+          res.sendFile(__dirname+'/views/thanks.html');
+  
+  
+  })
+
+
+
+
+  app.get('/',function(req,res){
+
+    User.find({},function(err,users){
+          if(err){
+              throw err;
+          }
+          res.json(users);
+    });
+  });
+
+
+
+
+
+  app.listen(port,function(err){
+    if (err){
+    throw err;
+    }
+    console.log(`server running at ${port}`);
+});
+
