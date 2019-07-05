8f7dd78b0ce05943aa7d67eba5c99c896659728e
knickheal75 <blitzkrieg0705@gmail.com>
2019-07-05 13:02:22 +0530

    yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,9 +1,10 @@
 console.log("starting app.js");
 const fs = require("fs");
-const os = require("os");
+const yargs = require("yargs");
+// const os = require("os");
 const notes = require("./notes.js");
 
-console.log(`Result:${notes.add(5, 8)}`);
+// console.log(`Result:${notes.add(5, 8)}`);
 
 // var res = notes.addNote();
 // console.log(res);
@@ -19,3 +20,20 @@ console.log(`Result:${notes.add(5, 8)}`);
 //     }
 //   }
 // );
+const argv = yargs.argv;
+var command = argv._[0];
+console.log("command:", command);
+console.log("process:", process.argv);
+console.log("yargs:", argv);
+
+if (command == "add") {
+  notes.addNote(argv.title, argv.body);
+} else if (command == "list") {
+  notes.getAll();
+} else if (command == "read") {
+  notes.getNote(argv.title);
+} else if (command == "remove") {
+  notes.removeNote(argv.title);
+} else {
+  console.log("command not recognised");
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,5 +1,27 @@
 console.log("starting notes.js");
 
-module.exports.add = (a, b) => {
-  return a + b;
+var addNote = (title, body) => {
+  console.log("adding note", title, body);
 };
+var getAll = () => {
+  console.log("getting all notes");
+};
+
+var getNote = title => {
+  console.log("reading note", title);
+};
+
+var removeNote = title => {
+  console.log("removing note", title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
+
+// // module.exports.add = (a, b) => {
+//   return a + b;
+// };
