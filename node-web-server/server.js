1b0612a1f336a5b170d82bc2bdbcc07a2af49abb
knickheal75 <blitzkrieg0705@gmail.com>
2019-07-10 11:15:34 +0530

    express middleware and hbs

---
+++ node-web-server/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const fs = require("fs");
+
+const hbs = require("hbs");
+
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) console.log("Unable to append to server.log");
+  });
+  next();
+});
+
+// app.use((req, res) => {
+//   res.render("maintenance.hbs");
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  //   res.send("<h1>hello express</h1>");
+  res.render("home.hbs", {
+    pageTitle: "HOME PAGE",
+    welcomeMessage: "welcome to home page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "about page",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "unable to handle request"
+  });
+});
+app.listen(3000, () => {
+  console.log("server is running at port 3000");
+});
