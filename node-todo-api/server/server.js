6fd9909cd0f3be311d384a7f52a331c0d03ad811
knickheal75 <blitzkrieg0705@gmail.com>
2019-07-13 07:40:53 +0530

    mocha test and db check with it

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var { mongoose } = require("./db/mongoose");
+var { Todo } = require("./models/todo");
+var { User } = require("./models/user");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/todos", (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = { app };
+
+// var newUser = new User({ email: "" });
+
+// newUser.save().then(
+//   doc => {
+//     console.log("saved email for user", doc);
+//   },
+//   err => {
+//     console.log("unable to save user email");
+//   }
+// );
+
+// var newTodo = new Todo({
+//   text: "cook dinner"
+// });
+
+// newTodo.save().then(
+//   doc => {
+//     console.log("saved todo", doc);
+//   },
+//   err => {
+//     console.log("unable to save todo");
+//   }
+// );
+
+// var otherTodo = new Todo({
+//   text: "   edit this video  "
+// });
+
+// otherTodo.save().then(
+//   doc => {
+//     console.log("saved todo", doc);
+//   },
+//   err => {
+//     console.log("unable to save todo");
+//   }
+// );
